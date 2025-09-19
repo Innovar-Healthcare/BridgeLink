4b4e92610740ff0ec8dd8585aedb30799cb428d0
Thai Tran <thaitran@innovarhealthcare.com>
2025-09-19 18:36:07 -0500

    Add template header

--- custom-extensions/message-trends/server/src/main/java/com/mirth/connect/plugins/messagetrends/server/servlet/support/TimeseriesStatisticsSerializer.java
+++ custom-extensions/message-trends/server/src/main/java/com/mirth/connect/plugins/messagetrends/server/servlet/support/TimeseriesStatisticsSerializer.java
@@ -1,3 +1,13 @@
+/*
+ *
+ * Copyright (c) Innovar Healthcare. All rights reserved.
+ *
+ * https://www.innovarhealthcare.com
+ *
+ * The software in this package is published under the terms of the MPL license a copy of which has
+ * been included with this distribution in the LICENSE.txt file.
+ */
+
 package com.mirth.connect.plugins.messagetrends.server.servlet.support;
 
 import java.util.List;
