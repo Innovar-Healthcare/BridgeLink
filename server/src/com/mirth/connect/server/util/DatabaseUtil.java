103409edc7605f587fc5ef7209fab90996d248bf
Thai Tran <thaitran@innovarhealthcare.com>
2025-10-08 11:18:30 -0500

    Revert DatabaseUtil to previous working version

--- server/src/com/mirth/connect/server/util/DatabaseUtil.java
+++ server/src/com/mirth/connect/server/util/DatabaseUtil.java
@@ -18,245 +18,249 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
-import java.util.logging.LogManager;
 
 import org.apache.commons.dbutils.DbUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import com.ibm.wsdl.util.StringUtils;
 import com.mirth.connect.donkey.server.data.DonkeyDaoException;
 
 public class DatabaseUtil {
-	private static Logger logger = LogManager.getLogger(DatabaseUtil.class);
+    private static Logger logger = LogManager.getLogger(DatabaseUtil.class);
 
-	public static void executeScript(String script, boolean ignoreErrors) throws Exception {
-		SqlSessionManager sqlSessionManger = SqlConfig.getInstance().getSqlSessionManager();
+    public static void executeScript(String script, boolean ignoreErrors) throws Exception {
+        SqlSessionManager sqlSessionManger = SqlConfig.getInstance().getSqlSessionManager();
 
-		Connection conn = null;
-		ResultSet resultSet = null;
-		Statement statement = null;
+        Connection conn = null;
+        ResultSet resultSet = null;
+        Statement statement = null;
 
-		try {
-			sqlSessionManger.startManagedSession();
-			conn = sqlSessionManger.getConnection();
+        try {
+            sqlSessionManger.startManagedSession();
+            conn = sqlSessionManger.getConnection();
 
-			/*
-			 * Set auto commit to false or an exception will be thrown when trying to
-			 * rollback
-			 */
-			conn.setAutoCommit(false);
+            /*
+             * Set auto commit to false or an exception will be thrown when trying to rollback
+             */
+            conn.setAutoCommit(false);
 
-			statement = conn.createStatement();
+            statement = conn.createStatement();
 
-			Scanner s = new Scanner(script);
+            Scanner s = new Scanner(script);
 
-			while (s.hasNextLine()) {
-				StringBuilder sb = new StringBuilder();
-				boolean blankLine = false;
+            while (s.hasNextLine()) {
+                StringBuilder sb = new StringBuilder();
+                boolean blankLine = false;
 
-				while (s.hasNextLine() && !blankLine) {
-					String temp = s.nextLine();
+                while (s.hasNextLine() && !blankLine) {
+                    String temp = s.nextLine();
 
-					if (temp.trim().length() > 0)
-						sb.append(temp + " ");
-					else
-						blankLine = true;
-				}
+                    if (temp.trim().length() > 0)
+                        sb.append(temp + " ");
+                    else
+                        blankLine = true;
+                }
 
-				// Trim ending semicolons so Oracle doesn't throw
-				// "java.sql.SQLException: ORA-00911: invalid character"
-				String statementString = StringUtils.removeEnd(sb.toString().trim(), ";");
+                // Trim ending semicolons so Oracle doesn't throw
+                // "java.sql.SQLException: ORA-00911: invalid character"
+                String statementString = StringUtils.removeEnd(sb.toString().trim(), ";");
 
-				if (statementString.length() > 0) {
-					try {
-						statement.execute(statementString);
-						conn.commit();
-					} catch (SQLException se) {
-						if (!ignoreErrors) {
-							throw se;
-						} else {
-							logger.error("Error was encountered and ignored while executing statement.", se);
-							conn.rollback();
-						}
-					}
-				}
-			}
+                if (statementString.length() > 0) {
+                    try {
+                        statement.execute(statementString);
+                        conn.commit();
+                    } catch (SQLException se) {
+                        if (!ignoreErrors) {
+                            throw se;
+                        } else {
+                            logger.error("Error was encountered and ignored while executing statement.", se);
+                            conn.rollback();
+                        }
+                    }
+                }
+            }
 
-		} catch (Exception e) {
-			throw new Exception(e);
-		} finally {
-			DbUtils.closeQuietly(statement);
-			DbUtils.closeQuietly(resultSet);
-			DbUtils.closeQuietly(conn);
-			sqlSessionManger.close();
-		}
-	}
+        } catch (Exception e) {
+            throw new Exception(e);
+        } finally {
+            DbUtils.closeQuietly(statement);
+            DbUtils.closeQuietly(resultSet);
+            DbUtils.closeQuietly(conn);
+            sqlSessionManger.close();
+        }
+    }
 
-	public static void executeScript(List<String> script, boolean ignoreErrors) throws Exception {
-		SqlSessionManager sqlSessionManger = SqlConfig.getInstance().getSqlSessionManager();
+    public static void executeScript(List<String> script, boolean ignoreErrors) throws Exception {
+        SqlSessionManager sqlSessionManger = SqlConfig.getInstance().getSqlSessionManager();
 
-		Connection conn = null;
-		ResultSet resultSet = null;
-		Statement statement = null;
+        Connection conn = null;
+        ResultSet resultSet = null;
+        Statement statement = null;
 
-		try {
-			sqlSessionManger.startManagedSession();
-			conn = sqlSessionManger.getConnection();
-			/*
-			 * Set auto commit to false or an exception will be thrown when trying to
-			 * rollback
-			 */
-			conn.setAutoCommit(false);
-			statement = conn.createStatement();
+        try {
+            sqlSessionManger.startManagedSession();
+            conn = sqlSessionManger.getConnection();
+            /*
+             * Set auto commit to false or an exception will be thrown when trying to rollback
+             */
+            conn.setAutoCommit(false);
+            statement = conn.createStatement();
 
-			for (String statementString : script) {
-				statementString = statementString.trim();
-				if (statementString.length() > 0) {
-					try {
-						statement.execute(statementString);
-						conn.commit();
-					} catch (SQLException se) {
-						if (!ignoreErrors) {
-							throw se;
-						} else {
-							logger.error("Error was encountered and ignored while executing statement.", se);
-							conn.rollback();
-						}
-					}
-				}
-			}
+            for (String statementString : script) {
+                statementString = statementString.trim();
+                if (statementString.length() > 0) {
+                    try {
+                        statement.execute(statementString);
+                        conn.commit();
+                    } catch (SQLException se) {
+                        if (!ignoreErrors) {
+                            throw se;
+                        } else {
+                            logger.error("Error was encountered and ignored while executing statement.", se);
+                            conn.rollback();
+                        }
+                    }
+                }
+            }
 
-		} catch (Exception e) {
-			throw new Exception(e);
-		} finally {
-			DbUtils.closeQuietly(statement);
-			DbUtils.closeQuietly(resultSet);
-			DbUtils.closeQuietly(conn);
-			sqlSessionManger.close();
-		}
-	}
+        } catch (Exception e) {
+            throw new Exception(e);
+        } finally {
+            DbUtils.closeQuietly(statement);
+            DbUtils.closeQuietly(resultSet);
+            DbUtils.closeQuietly(conn);
+            sqlSessionManger.close();
+        }
+    }
 
-	/**
-	 * Returns true if the statement exists in the SqlMap, false otherwise.
-	 * 
-	 * @param statement the statement, including the namespace
-	 * @return
-	 */
-	public static boolean statementExists(String statement) {
-		try {
-			SqlConfig.getInstance().getSqlSessionManager().getConfiguration().getMappedStatement(statement);
-		} catch (IllegalArgumentException e) {
-			// The statement does not exist
-			return false;
-		}
+    /**
+     * Returns true if the statement exists in the SqlMap, false otherwise.
+     * 
+     * @param statement
+     *            the statement, including the namespace
+     * @return
+     */
+    public static boolean statementExists(String statement) {
+        try {
+            SqlConfig.getInstance().getSqlSessionManager().getConfiguration().getMappedStatement(statement);
+        } catch (IllegalArgumentException e) {
+            // The statement does not exist
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Returns true if the statement exists in the SqlMap, false otherwise.
-	 * 
-	 * @param statement the statement, including the namespace
-	 * @param session   the SqlMapSession to use
-	 * @return
-	 */
-	public static boolean statementExists(String statement, SqlSession session) {
-		try {
-			session.getConfiguration().getMappedStatement(statement);
-		} catch (IllegalArgumentException e) {
-			// The statement does not exist
-			return false;
-		}
+    /**
+     * Returns true if the statement exists in the SqlMap, false otherwise.
+     * 
+     * @param statement
+     *            the statement, including the namespace
+     * @param session
+     *            the SqlMapSession to use
+     * @return
+     */
+    public static boolean statementExists(String statement, SqlSession session) {
+        try {
+            session.getConfiguration().getMappedStatement(statement);
+        } catch (IllegalArgumentException e) {
+            // The statement does not exist
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	public static List<String> joinSqlStatements(Collection<String> scripts) {
-		List<String> scriptList = new ArrayList<String>();
+    public static List<String> joinSqlStatements(Collection<String> scripts) {
+        List<String> scriptList = new ArrayList<String>();
 
-		for (String script : scripts) {
-			script = script.trim();
-			StringBuilder sb = new StringBuilder();
-			boolean blankLine = false;
-			Scanner scanner = new Scanner(script);
+        for (String script : scripts) {
+            script = script.trim();
+            StringBuilder sb = new StringBuilder();
+            boolean blankLine = false;
+            Scanner scanner = new Scanner(script);
 
-			while (scanner.hasNextLine()) {
-				String temp = scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String temp = scanner.nextLine();
 
-				if (temp.trim().length() > 0) {
-					sb.append(temp + " ");
-				} else {
-					blankLine = true;
-				}
+                if (temp.trim().length() > 0) {
+                    sb.append(temp + " ");
+                } else {
+                    blankLine = true;
+                }
 
-				if (blankLine || !scanner.hasNextLine()) {
-					scriptList.add(sb.toString().trim());
-					blankLine = false;
-					sb.delete(0, sb.length());
-				}
-			}
-		}
+                if (blankLine || !scanner.hasNextLine()) {
+                    scriptList.add(sb.toString().trim());
+                    blankLine = false;
+                    sb.delete(0, sb.length());
+                }
+            }
+        }
 
-		return scriptList;
-	}
+        return scriptList;
+    }
 
-	/**
-	 * Tell whether or not the given table exists in the database
-	 */
-	public static boolean tableExists(Connection connection, String tableName) {
-		ResultSet resultSet = null;
+    /**
+     * Tell whether or not the given table exists in the database
+     */
+    public static boolean tableExists(Connection connection, String tableName) {
+        ResultSet resultSet = null;
 
-		try {
-			DatabaseMetaData metaData = connection.getMetaData();
-			resultSet = metaData.getTables(null, null, tableName.toUpperCase(), new String[] { "TABLE" });
+        try {
+            DatabaseMetaData metaData = connection.getMetaData();
+            resultSet = metaData.getTables(null, null, tableName.toUpperCase(), new String[] {
+                    "TABLE" });
 
-			if (resultSet.next()) {
-				return true;
-			}
+            if (resultSet.next()) {
+                return true;
+            }
 
-			resultSet = metaData.getTables(null, null, tableName.toLowerCase(), new String[] { "TABLE" });
-			return resultSet.next();
-		} catch (SQLException e) {
-			throw new DonkeyDaoException(e);
-		} finally {
-			DbUtils.closeQuietly(resultSet);
-		}
-	}
+            resultSet = metaData.getTables(null, null, tableName.toLowerCase(), new String[] {
+                    "TABLE" });
+            return resultSet.next();
+        } catch (SQLException e) {
+            throw new DonkeyDaoException(e);
+        } finally {
+            DbUtils.closeQuietly(resultSet);
+        }
+    }
 
-	/**
-	 * Tell whether or not the given index exists in the database
-	 */
-	public static boolean indexExists(Connection connection, String tableName, String indexName) {
-		if (!tableExists(connection, tableName)) {
-			return false;
-		}
+    /**
+     * Tell whether or not the given index exists in the database
+     */
+    public static boolean indexExists(Connection connection, String tableName, String indexName) {
+        if (!tableExists(connection, tableName)) {
+            return false;
+        }
 
-		ResultSet resultSet = null;
+        ResultSet resultSet = null;
 
-		try {
-			DatabaseMetaData metaData = connection.getMetaData();
-			resultSet = metaData.getIndexInfo(null, null, tableName.toUpperCase(), false, false);
+        try {
+            DatabaseMetaData metaData = connection.getMetaData();
+            resultSet = metaData.getIndexInfo(null, null, tableName.toUpperCase(), false, false);
 
-			while (resultSet.next()) {
-				if (indexName.equalsIgnoreCase(resultSet.getString("INDEX_NAME"))) {
-					return true;
-				}
-			}
+            while (resultSet.next()) {
+                if (indexName.equalsIgnoreCase(resultSet.getString("INDEX_NAME"))) {
+                    return true;
+                }
+            }
 
-			resultSet = metaData.getIndexInfo(null, null, tableName.toLowerCase(), false, false);
+            resultSet = metaData.getIndexInfo(null, null, tableName.toLowerCase(), false, false);
 
-			while (resultSet.next()) {
-				if (indexName.equalsIgnoreCase(resultSet.getString("INDEX_NAME"))) {
-					return true;
-				}
-			}
+            while (resultSet.next()) {
+                if (indexName.equalsIgnoreCase(resultSet.getString("INDEX_NAME"))) {
+                    return true;
+                }
+            }
 
-			return false;
-		} catch (SQLException e) {
-			throw new DonkeyDaoException(e);
-		} finally {
-			DbUtils.closeQuietly(resultSet);
-		}
-	}
+            return false;
+        } catch (SQLException e) {
+            throw new DonkeyDaoException(e);
+        } finally {
+            DbUtils.closeQuietly(resultSet);
+        }
+    }
 }
